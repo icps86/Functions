0deffe30edca351c5174d1675affa52a8b857681
Ignacio Pezo <icps86@users.noreply.github.com>
2017-06-09 10:43:11 -0400

    Create krzycensuz.r

---
+++ krzycensuz.r
@@ -0,0 +1,19 @@
+krzycensuz <- function (x)
+  {
+  # Load Required Packages
+  require(ggplot2)
+  
+  #making pot
+  bars <- ggplot(x, aes(fill=county.name, height=.4, width=.05)) +
+    geom_vline(xintercept=0, color = "grey70", size=2, linetype = "solid") +
+    geom_tile(aes(x=dat$Pop.n, y="Population"), stat="identity") +
+    geom_tile(aes(x=dat$MHincome.n, y="Median Home Income"), stat="identity") + 
+    geom_tile(aes(x=dat$pov.rate.n, y="Poverty Rate"), stat="identity") +
+    geom_hline(yintercept=c(2.5,1.5), size = 3, color = "white" ) +
+    theme(legend.position = "top",
+          axis.title = element_blank(),
+          panel.border = element_blank(),
+          legend.title = element_blank())
+  return(bars)
+   
+}
