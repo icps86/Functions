c34ca5df0be5991bce6005c3cffd77d09978a24e
Ignacio Pezo <icps86@users.noreply.github.com>
2017-06-13 17:00:56 -0400

    Create krzycensuz.r

--- krzycensuz.r
+++ krzycensuz.r
@@ -4,13 +4,14 @@ krzycensuz <- function (x)
   require(ggplot2)
   require(gridExtra)
   options(scipen = 999)
+  
+  #the function will make three plots, save them into objects and stack them together using gridExtra
 
-  #making pot
+  #making the first plot
   bar.pop <- ggplot(x, aes(fill=county.name)) +
     geom_vline(xintercept=317309, color = "grey80", size=4, linetype = "solid") +
     geom_text(aes(x=317309, y="Population        ", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$Pop, y="Population        "), stat="identity", shape = 23, size = 8) +
-    
     #theme_bw() +
     xlim(4700,max(x$Pop)+1000) + 
     theme(legend.position = "none",
@@ -19,14 +20,13 @@ krzycensuz <- function (x)
           legend.title = element_blank(),
           panel.background = element_rect(colour = 'gray80'),
           panel.grid.minor = element_blank(), 
-          panel.grid.major =element_blank()))
+          panel.grid.major =element_blank())
   
+  #second plot
   bar.inc <- ggplot(x, aes(fill=county.name)) +
     geom_vline(xintercept=55276, color = "grey80", size=4, linetype = "solid") +
     geom_text(aes(x=55276, y="Median Income", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$MHincome, y="Median Income"), stat="identity", shape = 23, size = 8) +
-    
-
     #theme_bw() +
     xlim(34200,max(x$MHincome)+800) +
     theme(legend.position = "none",
@@ -35,13 +35,13 @@ krzycensuz <- function (x)
           legend.title = element_blank(),
           panel.background = element_rect(colour = 'gray80'),
           panel.grid.minor = element_blank(), 
-          panel.grid.major =element_blank()))
+          panel.grid.major =element_blank())
   
+  #third plot
   bar.pov <- ggplot(x, aes(fill=county.name)) +
     geom_vline(xintercept=13.8, color = "grey80", size=4, linetype = "solid") +
     geom_text(aes(x=13.8, y="Poverty Rate     ", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$pov.rate, y="Poverty Rate     "), stat="identity", shape = 23, size = 8) +
-
     #theme_bw() +
     xlim(5,max(x$pov.rate)+1) +
     theme(legend.position = "none",
@@ -50,8 +50,9 @@ krzycensuz <- function (x)
           legend.title = element_blank(),
           panel.background = element_rect(colour = 'gray80'),
           panel.grid.minor = element_blank(), 
-          panel.grid.major =element_blank()))
+          panel.grid.major =element_blank())
   
+  #making a legend with a plot.
   leg <- ggplot(x, aes(fill=county.name)) +
   geom_point(aes(x=2:(length(levels(x$county.name))+1), y=""), stat="identity", shape = 23, size = 8) +
   theme_bw() +
@@ -68,9 +69,9 @@ krzycensuz <- function (x)
           axis.ticks=element_blank(),
           panel.grid=element_blank(),
           panel.border = element_blank(),
-          legend.title = element_blank()
-          )
-     
+          legend.title = element_blank())
+  
+  #compiling everything for output
   bars <- grid.arrange(bar.pop, bar.inc, bar.pov, leg, nrow= 4, ncol=1, heights=c(1,1,1,1), widths=c(1))
   
   return(bars)
