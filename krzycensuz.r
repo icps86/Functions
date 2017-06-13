a5a2bd190710ce37bf16be863c36e1c9b71a5ba2
Ignacio Pezo <icps86@users.noreply.github.com>
2017-06-13 10:33:42 -0400

    Update krzycensuz.r

--- krzycensuz.r
+++ krzycensuz.r
@@ -1,4 +1,6 @@
 krzycensuz <- function (x)
+
+krzycensuz <- function (x)
 {
   # Load Required Packages
   require(ggplot2)
@@ -7,25 +9,37 @@ krzycensuz <- function (x)
 
   #making pot
   bar.pop <- ggplot(x, aes(fill=county.name)) +
-    #geom_vline(xintercept=0, color = "grey70", size=2, linetype = "solid") +
+    geom_vline(xintercept=317309, color = "grey80", size=4, linetype = "solid") +
+    geom_text(aes(x=317309, y="Population        ", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$Pop, y="Population        "), stat="identity", shape = 23, size = 8) +
+    
     #theme_bw() +
+    xlim(4700,max(x$Pop)+1000) + 
     theme(legend.position = "none",
           axis.title = element_blank(),
           panel.border = element_blank(),
           legend.title = element_blank())
   
   bar.inc <- ggplot(x, aes(fill=county.name)) +
+    geom_vline(xintercept=55276, color = "grey80", size=4, linetype = "solid") +
+    geom_text(aes(x=55276, y="Median Income", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$MHincome, y="Median Income"), stat="identity", shape = 23, size = 8) +
+    
+
     #theme_bw() +
+    xlim(34200,max(x$MHincome)+800) +
     theme(legend.position = "none",
           axis.title = element_blank(),
           panel.border = element_blank(),
           legend.title = element_blank())
   
   bar.pov <- ggplot(x, aes(fill=county.name)) +
+    geom_vline(xintercept=13.8, color = "grey80", size=4, linetype = "solid") +
+    geom_text(aes(x=13.8, y="Poverty Rate     ", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$pov.rate, y="Poverty Rate     "), stat="identity", shape = 23, size = 8) +
+
     #theme_bw() +
+    xlim(5,max(x$pov.rate)+1) +
     theme(legend.position = "none",
           axis.title = element_blank(),
           panel.border = element_blank(),
@@ -35,7 +49,7 @@ krzycensuz <- function (x)
   geom_point(aes(x=2:(length(levels(x$county.name))+1), y=""), stat="identity", shape = 23, size = 8) +
   theme_bw() +
   coord_cartesian(xlim = 1:(length(levels(x$county.name))+2)) +
-  geom_text(aes(x=2:(length(levels(x$county.name))+1), y=.7, label = paste0(levels(x$county.name))),
+  geom_text(aes(x=2:(length(levels(x$county.name))+1), y=.8, label = paste0(levels(x$county.name))),
             size= 4,
             hjust = .5,
             vjust = .5,
