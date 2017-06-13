a04c962cb59033f9abb89c713da9c955f7ec14d6
Ignacio Pezo <icps86@users.noreply.github.com>
2017-06-13 17:39:14 -0400

    Update krzycensuz.r

--- krzycensuz.r
+++ krzycensuz.r
@@ -1,4 +1,3 @@
-
 krzycensuz <- function (x)
 {
   # Load Required Packages
@@ -9,7 +8,7 @@ krzycensuz <- function (x)
   #the function will make three plots, save them into objects and stack them together using gridExtra
 
   #making the first plot
-  bar.pop <- ggplot(x, aes(fill=county.name)) +
+  bar.pop <- ggplot(x, aes(fill=county.name, color = county.name)) +
     geom_vline(xintercept=317309, color = "grey80", size=4, linetype = "solid") +
     geom_text(aes(x=317309, y="Population        ", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$Pop, y="Population        "), stat="identity", shape = 23, size = 8) +
@@ -27,7 +26,7 @@ krzycensuz <- function (x)
           #panel.grid.minor.x = element_line(color = "grey80"))
   
   #second plot
-  bar.inc <- ggplot(x, aes(fill=county.name)) +
+  bar.inc <- ggplot(x, aes(fill=county.name, color = county.name)) +
     geom_vline(xintercept=55276, color = "grey80", size=4, linetype = "solid") +
     geom_text(aes(x=55276, y="Median Income", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$MHincome, y="Median Income"), stat="identity", shape = 23, size = 8) +
@@ -45,7 +44,7 @@ krzycensuz <- function (x)
           #panel.grid.minor.x = element_line(color = "grey80"))
   
   #third plot
-  bar.pov <- ggplot(x, aes(fill=county.name)) +
+  bar.pov <- ggplot(x, aes(fill=county.name, color = county.name)) +
     geom_vline(xintercept=13.8, color = "grey80", size=4, linetype = "solid") +
     geom_text(aes(x=13.8, y="Poverty Rate     ", label= "NY Average"), size = 3, colour = "grey40", angle = 90) +
     geom_point(aes(x=x$pov.rate, y="Poverty Rate     "), stat="identity", shape = 23, size = 8) +
@@ -64,11 +63,11 @@ krzycensuz <- function (x)
     
   
   #making a legend with a plot.
-  leg <- ggplot(x, aes(fill=county.name)) +
+  leg <- ggplot(x, aes(fill=county.name, color = county.name)) +
   geom_point(aes(x=2:(length(levels(x$county.name))+1), y=""), stat="identity", shape = 23, size = 8) +
   theme_minimal() +
   coord_cartesian(xlim = 1:(length(levels(x$county.name))+2)) +
-  geom_text(aes(x=2:(length(levels(x$county.name))+1), y=.8, label = paste0(levels(x$county.name))),
+  geom_text(aes(x=2:(length(levels(x$county.name))+1), y=.7, label = paste0(levels(x$county.name))),
             size= 4,
             hjust = .5,
             vjust = .5,
